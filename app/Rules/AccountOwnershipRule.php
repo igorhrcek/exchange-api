02991479a96be308dfc16d839316a8c997fd5718
Igor Hrček <igor@hrcek.rs>
2023-06-04 18:42:41 +0200

    feat: Implemented rule that verifies account ownership#

---
+++ app/Rules/AccountOwnershipRule.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use App\Models\Account;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class AccountOwnershipRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $user = Auth::user();
+
+        if (Account::get($value)->user_id !== $user->id) {
+            $fail('The account belongs to another user. You are not authorized to do this transaction.');
+        }
+    }
+}
