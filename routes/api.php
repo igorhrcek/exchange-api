d19f80a846fd734de54e58467f841d07a92c71eb
Igor Hrček <igor@hrcek.rs>
2023-06-04 17:43:37 +0200

    feat: Implemented API routing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CurrencyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/currency', [CurrencyController::class, 'index']);
+
+Route::post('/user', [UserController::class, 'store']);
+
+Route::middleware('auth:sanctum')->post('/account', [AccountController::class, 'store']);
+Route::middleware('auth:sanctum')->get('/account', [AccountController::class, 'show']);
+
+Route::middleware('auth:sanctum')->post('/transaction', [TransactionController::class, 'store']);
+Route::middleware('auth:sanctum')->get('/transaction', [TransactionController::class, 'show']);
+
