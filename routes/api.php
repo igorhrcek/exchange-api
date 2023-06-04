71546f5ddb60ea7c97f7622d1b136143e4fecf8e
Igor Hrček <igor@hrcek.rs>
2023-06-04 21:59:30 +0200

    feat: Added name to routes

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/currency', [CurrencyController::class, 'index']);
+Route::get('/currency', [CurrencyController::class, 'index'])->name('currency.get');
 
-Route::post('/user', [UserController::class, 'store']);
+Route::post('/user', [UserController::class, 'store'])->name('user.create');
 
 Route::middleware('auth:sanctum')->post('/account', [AccountController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/accounts', [AccountController::class, 'index']);
