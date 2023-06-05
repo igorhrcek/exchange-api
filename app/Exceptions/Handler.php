4e451a789ede598ea0c900044180c56ed792d447
Igor Hrček <igor@hrcek.rs>
2023-06-05 22:43:20 +0200

    feat: Added handling of additional exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,32 +34,33 @@ class Handler extends ExceptionHandler
             ], Response::HTTP_BAD_REQUEST);
         });
 
+        $this->renderable(function (NotEnoughBalanceException $e, $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'You do not have enough balance on the account.'
+            ], Response::HTTP_BAD_REQUEST);
+        });
+
         $this->renderable(function (NotFoundHttpException $e, $request) {
             if ($request->is('api/account/*')) {
                 return response()->json([
                     'message' => 'Account not found.'
-                ], 404);
+                ], Response::HTTP_NOT_FOUND);
             }
 
             //Fail if transaction reference provided is incorrect
             if ($request->is('api/transaction/*')) {
                 return response()->json([
                     'message' => 'Transaction not found.'
-                ], 404);
+                ], Response::HTTP_NOT_FOUND);
             }
 
             //Fail if account UUID provided is incorrect
             if ($request->is('api/transactions/*')) {
                 return response()->json([
                     'message' => 'Account not found.'
-                ], 404);
+                ], Response::HTTP_NOT_FOUND);
             }
-        });
-
-        $this->renderable(function (NotEnoughBalanceException $e, $request) {
-            return response()->json([
-                'message' => 'You do not have enough balance on the account.'
-            ], 400);
         });
     }
 }
