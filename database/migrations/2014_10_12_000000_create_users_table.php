033aa767dae131f1a8f697a4c720f866b73e63ef
Igor Hrček <igor@hrcek.rs>
2023-06-05 18:26:29 +0200

    feat: Returned missing columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
         });
